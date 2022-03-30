19df949533637272e7548b1d64b22442e8f620af
paulbradfo <90589413+paulbradfo@users.noreply.github.com>
2022-03-30 16:50:45 -0600

    Add project files.

---
+++ Mission13/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Mission13.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BowlersDbContext _context { get; set; }
+
+        public HomeController(BowlersDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var bowl = _context.Bowlers.ToList();
+
+            return View(bowl);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int bowlerid)
+        {
+            var blr = _context.Bowlers
+                .Single(x => x.BowlerID == bowlerid);
+            return View("Bowlinfo", blr);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Bowler blr)
+        {
+            _context.Update(blr);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int bowlerid)
+        {
+            var blr = _context.Bowlers.Single(x => x.BowlerID == bowlerid);
+
+            _context.Bowlers.Remove(blr);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Team(int teamid)
+        {
+            var blrs = _context.Bowlers.FromSqlRaw("SELECT * FROM Bowlers WHERE TeamID =" + teamid).ToList();
+            return View("Index", blrs);
+        }
+
+        [HttpGet]
+        public IActionResult AddBowl()
+        {
+            return View("Bowlinfo");
+        }
+
+        [HttpPost]
+        public IActionResult AddBowl(Bowler blr)
+        {
+            _context.Update(blr);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+
---
+++ Mission13/Models/Bowler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Models
+{
+    public class Bowler
+    {
+        [Key]
+        [Required]
+        public int BowlerID { get; set; }
+
+        public string BowlerLastName { get; set; }
+
+        public string BowlerFirstName { get; set; }
+
+        [StringLength(1)]
+        public string BowlerMiddleInit { get; set; }
+
+        public string BowlerAddress { get; set; }
+
+        public string BowlerCity { get; set; }
+
+        [StringLength(2)]
+        public string BowlerState { get; set; }
+
+        public string BowlerZip { get; set; }
+
+        public string BowlerPhoneNumber { get; set; }
+
+        [Required]
+        public int TeamID { get; set; }
+    }
+}
